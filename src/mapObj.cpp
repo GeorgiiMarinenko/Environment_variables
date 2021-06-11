c034e6f0b84c7baf3b38fc48fe754921a0e093af
Георгий Мариненко <georgijmarinenko@MacBook-Pro-Georgij.local>
2021-06-12 00:06:30 +0300

    Dictionary parser + key parser from text.txt

--- src/mapObj.cpp
+++ src/mapObj.cpp
@@ -15,12 +15,13 @@ map<string, string> mapObj::setMap()
 
 	mapObject.envVariablesMap = parser.dctParser();
 	//печать содержимого в map из файла dictionary.txt
-	// for (mapObject.it = mapObject.envVariablesMap.begin(); mapObject.it != mapObject.envVariablesMap.end(); mapObject.it++)
-	// {
-	// 	cout << mapObject.it->first << " : " << mapObject.it->second << endl;
-	// }
+	for (mapObject.it = mapObject.envVariablesMap.begin(); mapObject.it != mapObject.envVariablesMap.end(); mapObject.it++)
+	{
+		cout << mapObject.it->first << " : " << mapObject.it->second << endl;
+	}
 	// cout << "Key: " << currentArg.Key << "\nValue: " << currentArg.Value << endl;
 	keyDetector obj1;
+	cout << "\nkeys from text.txt:" << endl << endl;
 	obj1.findKeyFromText();
 	return (mapObject.envVariablesMap);
 }
