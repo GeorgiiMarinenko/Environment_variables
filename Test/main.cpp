88b3b73ec6ff125b493d86ac0464578781d58b12
Георгий Мариненко <gmarinenko@mail.ru>
2021-06-29 23:03:42 +0300

    Check arguments in main.cpp

--- Test/main.cpp
+++ Test/main.cpp
@@ -9,6 +9,11 @@ int main(int argc, char* argv[])
 {
 	//Инициализация map & string
 	//
+	if (argc != 3)
+	{
+		cout << "Put down the right number of arguments:\n1. 'dictionaryFile'.txt\n2. 'textFile.txt\n";
+		return (-1);
+	}
 	string dictFilePath =  string(argv[1]);
 	string textFilePath =  string(argv[2]);
 	// cout << dictFilePath << '\n' << textFilePath << endl;
