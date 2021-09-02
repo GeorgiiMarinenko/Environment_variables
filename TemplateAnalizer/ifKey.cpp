a4f169631cb94289ad1afdb849454bb868d61b28
Георгий Мариненко <gmarinenko@mail.ru>
2021-09-02 16:35:41 +0300

    V1.1. with if\else modules

--- TemplateAnalizer/ifKey.cpp
+++ TemplateAnalizer/ifKey.cpp
@@ -5,6 +5,13 @@
 #include <cstdlib>
 using namespace std;
 
+int	signSkipper()
+{
+	int	curPos = 0;
+
+	return (curPos);
+}
+
 /*
 / == - 0
 / != - 1
@@ -56,6 +63,7 @@ int signDetector(int strSize, int* p_strIndex, const string& str)
 	*p_strIndex = curPos;
 	return (sign);
 }
+
 string findKeyValue(map<string, string> mp, const string& variable)
 {
 	map<string, string>::iterator	it;
@@ -66,8 +74,72 @@ string findKeyValue(map<string, string> mp, const string& variable)
 		cout << "Значение переменной " << variable << " не найдено" << endl;
 		return ("----------->Incorrect value");
 	}
-	cout << "varValue: " << it->second << endl;
+	// cout << "varValue: " << it->second << endl;
 	return (it->second);
+}
+
+string ifCase(int* p_strIndex, const string& str)
+{
+	int		curPos = *p_strIndex;
+	string	ifCase;
+	bool	wasAlpha = false;
+	while (str[curPos] != '{')
+		curPos++;
+	while (str[curPos + 1] != '}')
+	{
+		curPos++;
+		if ((str[curPos] == '\n') && !wasAlpha)
+		{
+			wasAlpha = true;
+			continue;
+		}
+		if ((str[curPos] == ' ') || (str[curPos] == '\t'))
+			continue;
+		ifCase += str[curPos];
+	}
+	cout << "ifKeyProc " << ifCase <<  endl;
+	return (ifCase);
+}
+
+string elseCase(int* p_strIndex, const string& str)
+{
+	string	elseCase;
+	bool	wasAlpha = false;
+	string::size_type start;
+	int		curPos = 0;
+	(void)p_strIndex;
+
+	start = str.find("else");
+	curPos = (int)start;
+	while (str[curPos] != '{')
+		curPos++;
+	while (str[curPos + 1] != '}')
+	{
+		curPos++;
+		if ((str[curPos] == '\n') && !wasAlpha)
+		{
+			wasAlpha = true;
+			continue;
+		}
+		if ((str[curPos] == ' ') || (str[curPos] == '\t'))
+			continue;
+		elseCase += str[curPos];
+	}
+	// curPos += 3; // позиция elsE
+	cout << "else: " << str[curPos] << endl;
+	return (elseCase);
+}
+
+string solution(bool isEqual, int* p_strIndex, const string& str)
+{
+	string	res;
+	if (isEqual)
+		res = ifCase(p_strIndex, str);
+	else if ((str.find("else") != string::npos) && (!isEqual))
+		res = elseCase(p_strIndex, str);
+	else
+		res = '\r';
+	return (res);
 }
 string varFinder(const string& str, map<string, string> mp)
 {
@@ -80,8 +152,6 @@ string varFinder(const string& str, map<string, string> mp)
 	int		*p_curPos = NULL;
 	int		strSize = 0;
 	int		sign = -1;
-	// int		length = 0;
-	// int		finish = 0;
 	string::size_type start;
 
 	strSize = str.length();
@@ -89,60 +159,46 @@ string varFinder(const string& str, map<string, string> mp)
 	if (start != string::npos)
 	{
 		curPos = (int)start;
-		while (curPos < strSize)
+		curPos++;
+		/*
+		if [var == val]
 		{
-			curPos++;
-			while (curPos < strSize)
-			{
-				curPos++;
-				/*
-				if [var == val]
-				{
-					...
-				}
-				else
-				{
-					...
-				}
-				*/
-				while ((str[curPos - 1] != '[') && (curPos < strSize))
-					curPos++;
-				while ((str[curPos] == ' ') && (curPos < strSize))
-					curPos++;
-				while ((str[curPos] >= 'a') && (str[curPos] <= 'z'))
-				{
-					variable += str[curPos];
-					curPos++;
-				}
-				varValue = findKeyValue(mp, variable); // Определение значения переменной
-				p_curPos = &curPos;
-				sign = signDetector(strSize, p_curPos, str); // Определение кода знака сравнения
-				// cout << "-- " << str[*p_curPos] << endl;
-				curPos = *p_curPos;
-				while ((str[curPos] == ' ') && (curPos < strSize))
-					curPos++;
-				// Получение правой величины для сравнения из if []
-				while (((str[curPos] >= ' ') && (str[curPos] <= '~') && (str[curPos] != ']'))
-				&& (curPos < strSize))
-				{
-					origValue += str[curPos];
-					curPos++;
-				}
-				// cout << "Orig value: " << origValue << endl;
-				// Сравнение
-				if (strcmp(varValue.c_str(), origValue.c_str()) == 0)
-					isEqual = true;
-				// cout << "compar: " << isEqual << endl;
-
-				break;
-			}
-			break;
+			...
 		}
+		else
+		{
+			...
+		}
+		*/
+		while ((str[curPos - 1] != '[') && (curPos < strSize))
+			curPos++;
+		while ((str[curPos] == ' ') && (curPos < strSize))
+			curPos++;
+		while ((str[curPos] >= 'a') && (str[curPos] <= 'z'))
+		{
+			variable += str[curPos];
+			curPos++;
+		}
+		varValue = findKeyValue(mp, variable); // Определение значения переменной
+		p_curPos = &curPos;
+		sign = signDetector(strSize, p_curPos, str); // Определение кода знака сравнения
+		// cout << "-- " << str[*p_curPos] << endl;
+		curPos = *p_curPos;
+		while ((str[curPos] == ' ') && (curPos < strSize))
+			curPos++;
+		// Получение правой величины для сравнения из if []
+		while (((str[curPos] >= ' ') && (str[curPos] <= '~') && (str[curPos] != ']'))
+		&& (curPos < strSize))
+		{
+			origValue += str[curPos];
+			curPos++;
+		}
+		*p_curPos = curPos;
+		// cout << "Orig value: " << origValue << endl;
+		// Сравнение
+		if (strcmp(varValue.c_str(), origValue.c_str()) == 0)
+			isEqual = true;
+		// cout << "compar: " << isEqual << endl;
 	}
-	return variable;
-}
-
-int ifKeyProccess()
-{
-	return (0);
+	return (solution(isEqual, p_curPos, str));
 }
